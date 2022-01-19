76ff568b674e017ce9c901753237627cffe2b9b8
Guilherme Pereira <94808024+Guilhep@users.noreply.github.com>
2022-01-19 18:32:09 -0300

    Update Js
    Versão 1.1

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
 var navBtn = document.querySelector('.nav-toggle');
 var navList = document.querySelector('.navList');
 var navLinks = document.querySelectorAll('.navList li')
-
-
-
-navBtn.addEventListener('click', () => {
-    navList.classList.toggle('active');
-})
-
-
-
-
-navLinks.forEach((e) => {
-    e.addEventListener('click',() => {
-        navList.classList.remove('active')
-    })
-})
-
-
-
-
-function scrollHeader(){
-    const nav = document.querySelector('.nav')
-    
-    if(this.scrollY >= 80){
-         nav.classList.add('scroll-header');
-    } else {
-        nav.classList.remove('scroll-header');
-    }
-}
-window.addEventListener('scroll', scrollHeader)
